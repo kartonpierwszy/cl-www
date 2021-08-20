f33ea2c6507fe3ef48d92e76ea60f126ff59e045
Karton Pierwszy <kartonpierwszy@gmail.com>
2021-08-20 11:33:34 +0200

    Utworzone pliki php dla 1. menu top, 2. menu bottom i załadowane do strony głównej index.php

---
+++ build/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Rafał Mikulski" />
+    <meta name="description" content="cookielookiepl" />
+    <meta name="keywords" content="makijaż permanentny" />
+
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="styles/layouts/main-page.css"
+    />
+    <!-- <link rel="stylesheet" type="text/css" href="styles/layouts/nav-top.css" />
+    <link rel="stylesheet" type="text/css" href="styles/layouts/nav-bottom.css" /> -->
+
+    <!-- <link rel="stylesheets" type="text/css" href="vendors/css/normalize.css" /> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- <script src="utils/less.js"></script> -->
+
+    <title>Cookielookie</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <div class="top">
+          <?php include ('nav-top.php'); ?>
+        </div>
+        <div class="bottom">
+          <?php include ('nav-bottom.php'); ?>
+        </div>
+      </nav>
+      <div class="hero-baner">
+        
+        
+
+      </div>
+    </header>
+  </body>
+</html>
