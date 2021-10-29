3dd547a41711852b2266148258a0b044656b760b
Karton Pierwszy <kartonpierwszy@gmail.com>
2021-10-29 14:34:34 +0200

    New content on 'szkolenia' pages

--- build/script/scripts.js
+++
@@ -1,7 +0,0 @@
-const mobileNav = document.querySelector('ul');
-const burgerIcon = document.querySelector('.burger');
-
-burgerIcon.addEventListener('click', function () {
-  mobileNav.classList.toggle('active');
-  burgerIcon.classList.toggle('active');
-});
