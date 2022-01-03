69bab3c77f609848c569de71ea24accaeba1142d
Karton Pierwszy <kartonpierwszy@gmail.com>
2022-01-03 10:08:20 +0100

    Udate everything.

--- build/script/scripts.js
+++ build/script/scripts.js
@@ -4,3 +4,11 @@ const navUL = document.getElementById('nav-ul');
 hamburger.addEventListener('click', () => {
   navUL.classList.toggle('show');
 });
+
+// BUTTONS ANIMATION
+const btn = document.getElementById('btn');
+const borderTopLeft = getElementById('border-top-left:hover');
+
+btn.addEventListener('mouseover', () => {
+  borderTopLeft.classList.toggle('show');
+});
