092381d6f2a024eea56db480baa2a07de0892b56
Karton Pierwszy <kartonpierwszy@gmail.com>
2022-01-29 07:06:18 +0100

    OK

--- build/script/scripts.js
+++ build/script/scripts.js
@@ -1,16 +1,44 @@
-const hamburger = document.querySelector('.hamburger');
-const Menu = document.querySelector('.menu');
-const subMenu = document.querySelector('.sub-menu');
+// const hamburger = document.querySelector('.hamburger');
+// const Menu = document.querySelector('.menu');
+// const subMenu = document.querySelector('.sub-menu');
 
-hamburger.addEventListener('click', function () {
-  Menu.classList.toggle('active');
-  hamburger.classList.toggle('active');
-  // console.log('ok');
-});
+// hamburger.addEventListener('click', function () {
+//   Menu.classList.toggle('active');
+//   hamburger.classList.toggle('active');
+// });
 
-document.querySelectorAll('.nav-link').forEach((n) =>
-  n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    Menu.classList.remove('active');
-  })
-);
+// document.querySelectorAll('.nav-link').forEach((n) =>
+//   n.addEventListener('click', () => {
+//     hamburger.classList.remove('active');
+//     Menu.classList.remove('active');
+//   })
+// );
+//finish first v
+
+// OK
+const openCloseNavMenu = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const subSzkolenia = document.querySelector('.sub-szkolenia');
+const subMenuSzkolenia = document.querySelector('.sub-menu-szkolenia');
+const subZabiegi = document.querySelector('.sub-zabiegi');
+const subMenuZabiegi = document.querySelector('.sub-menu-zabiegi');
+// const screenSize = 769;
+
+// Open/close mobile menu on click hamburger icon
+openCloseNavMenu.addEventListener('click', toggleNav);
+
+function toggleNav() {
+  navMenu.classList.toggle('open');
+  openCloseNavMenu.classList.toggle('active');
+}
+
+// Collapse submenu for szkolenia & zabiegi link
+subSzkolenia.addEventListener('click', subMenuSzkoleniaCollapse);
+subZabiegi.addEventListener('click', subMenuZabiegiCollapse);
+
+function subMenuSzkoleniaCollapse() {
+  subMenuSzkolenia.classList.toggle('active');
+}
+function subMenuZabiegiCollapse() {
+  subMenuZabiegi.classList.toggle('active');
+}
