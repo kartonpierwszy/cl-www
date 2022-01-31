6b43644ea2308914cfa24325a265354d6ae0e7fa
Karton Pierwszy <kartonpierwszy@gmail.com>
2022-01-31 13:26:55 +0100

    Git repaired, project OK

--- build/script/scripts.js
+++ build/script/scripts.js
@@ -1,27 +1,13 @@
-// const hamburger = document.querySelector('.hamburger');
-// const Menu = document.querySelector('.menu');
-// const subMenu = document.querySelector('.sub-menu');
-
-// hamburger.addEventListener('click', function () {
-//   Menu.classList.toggle('active');
-//   hamburger.classList.toggle('active');
-// });
-
-// document.querySelectorAll('.nav-link').forEach((n) =>
-//   n.addEventListener('click', () => {
-//     hamburger.classList.remove('active');
-//     Menu.classList.remove('active');
-//   })
-// );
-//finish first v
-
-// OK
 const openCloseNavMenu = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const Menu = document.querySelector('.menu');
 const subSzkolenia = document.querySelector('.sub-szkolenia');
 const subMenuSzkolenia = document.querySelector('.sub-menu-szkolenia');
 const subZabiegi = document.querySelector('.sub-zabiegi');
 const subMenuZabiegi = document.querySelector('.sub-menu-zabiegi');
+const arrowSzkolenia = document.querySelector('.arrow-szkolenia');
+const arrowZabiegi = document.querySelector('.arrow-zabiegi');
+// const arrow = document.querySelector('.arrow');
 // const screenSize = 769;
 
 // Open/close mobile menu on click hamburger icon
@@ -38,7 +24,48 @@ subZabiegi.addEventListener('click', subMenuZabiegiCollapse);
 
 function subMenuSzkoleniaCollapse() {
   subMenuSzkolenia.classList.toggle('active');
+  arrowSzkolenia.classList.toggle('active');
 }
 function subMenuZabiegiCollapse() {
   subMenuZabiegi.classList.toggle('active');
+  arrowZabiegi.classList.toggle('active');
 }
+
+// Closing menu, when click on any links from menu
+// document.querySelectorAll('.menu').forEach((n) =>
+//   n.addEventListener('click', () => {
+//     navMenu.classList.remove('open');
+//     subMenuSzkolenia.remove('active');
+//     subMenuZabiegi.remove('active');
+//   })
+// );
+
+function closeMenu() {
+  document.querySelectorAll('.hamburger', '.menu').forEach((n) =>
+    n.addEventListener('click', () => {
+      navMenu.classList.remove('open');
+      subMenuSzkolenia.remove('active');
+      subMenuZabiegi.remove('active');
+    })
+  );
+}
+
+// function collapseNavMenu() {
+//   navMenu.querySelector('.navMenu.open').classList.remove('.open');
+// }
+
+// function resizeFix() {
+//   if (navMenu.classList.contains('open')) {
+//     collapseNavMenu();
+//   }
+//   if (navMenu.querySelector('.navMenu.open')) {
+//     collapseNavMenu();
+//   }
+// }
+
+// window.addEventListener('resize', function () {
+//   if (this.innerWidth > screenSize) {
+//     resizeFix();
+//     // navMenu.classList.remove('open');
+//   }
+// });
